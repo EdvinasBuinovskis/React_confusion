8929db322b891270cac1fb7c364254a7521cef10
Edvinas Buinovskis <edvinas.buinovskis@stud.vgtu.lt>
2020-07-25 19:57:07 +0300

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -98,7 +99,7 @@ class CommentForm extends Component {
             return(
                 <div className="col-12 col-md-5 m-1">
                     <Card>
-                        <CardImg top src={dish.image} alt={dish.name}/>
+                        <CardImg top src={baseUrl + dish.image} alt={dish.name}/>
                         <CardBody>
                             <CardTitle>{dish.name}</CardTitle>
                             <CardText>{dish.description}</CardText>
@@ -160,7 +161,7 @@ class CommentForm extends Component {
         }
         else if (props.dish != null){
             return (
-                <div class="container">
+                <div className="container">
                     <div className="row">
                         <Breadcrumb>
                             <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>

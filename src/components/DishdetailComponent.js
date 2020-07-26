0e3738d8320fc9d316e705520da0ee86246d2fbb
Edvinas Buinovskis <edvinas.buinovskis@stud.vgtu.lt>
2020-07-26 18:33:00 +0300

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -98,13 +99,18 @@ class CommentForm extends Component {
         if (dish != null) {
             return(
                 <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg top src={baseUrl + dish.image} alt={dish.name}/>
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
+                    <FadeTransform in 
+                        transformProps={{
+                            exitTransform: 'scale(0.5) translateY(-50%)'
+                    }}>
+                        <Card>
+                            <CardImg top src={baseUrl + dish.image} alt={dish.name}/>
+                            <CardBody>
+                                <CardTitle>{dish.name}</CardTitle>
+                                <CardText>{dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </FadeTransform>
                 </div>
             );
         }
@@ -117,21 +123,26 @@ class CommentForm extends Component {
 
     function RenderComments({comments, postComment, dishId}) {
         if (comments !=null) {
-            const usercomments = comments.map((usercomment) =>
-                <div key ={usercomment.id}>
-                    <li>
-                        <p>{usercomment.comment}</p>
-                        <p>-- {usercomment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(usercomment.date)))}</p>
-                    </li>
+            return (
+                <div className="col-12 col-md-5 m-1">
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                        <Stagger in>
+                            {comments.map((usercomment) => {
+                                return (
+                                    <Fade in>
+                                        <li key ={usercomment.id}>
+                                        <p>{usercomment.comment}</p>
+                                        <p>-- {usercomment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(usercomment.date)))}</p>
+                                        </li>
+                                    </Fade>
+                                );
+                            })}
+                        </Stagger>
+                    </ul>
+                    <CommentForm dishId={dishId} postComment={postComment} />
                 </div>
-                );
-                return (
-                    <div className="col-12 col-md-5 m-1">
-                        <h4>Comments</h4>
-                        <ul className="list-unstyled">{usercomments}</ul>
-                        <CommentForm dishId={dishId} postComment={postComment} />
-                    </div>
-                );
+            );
         }
         else {
             return(
